3203a16612c34682b8a145a3af78868ce35fcb4d
alykhan02 <aversi07@gmail.com>
2024-02-01 15:58:03 -0500

    server.js typo

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -104,6 +104,17 @@ app.delete("/deleteNote/:noteId", express.json(), async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 })
+
+// Delete all notes
+app.delete("/deleteAllNotes", express.json(), async (req, res) => {
+  try {
+    const collection = db.collection(COLLECTIONS.notes);
+    const data = await collection.deleteMany({});
+    return res.json({ response: data.deletedCount + " deleted."});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
   
 // Patch a note
 app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
@@ -126,7 +137,6 @@ app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
     // Find note with given ID
     const collection = db.collection(COLLECTIONS.notes);
     const data = await collection.updateOne({
-      username: decoded.username,
       _id: new ObjectId(noteId),
     }, {
       $set: {
@@ -145,13 +155,3 @@ app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
     res.status(500).json({error: error.message})
   }
 })
-
-app.delete("/deleteAllNotes", express.json(), async (req, res) => {
-  try {
-    const collection = db.collection(COLLECTIONS.notes);
-    const result = await collection.deleteMany({});
-    res.json({ response: `${result.deletedCount} notes deleted.` });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
